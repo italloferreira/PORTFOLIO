2b3d4dab650e15e246c169b2d28e35512c7dcf51
Itallo Ferreira <italloferreira2208@gmail.com>
2026-03-22 16:34:53 -0300

    refactor: Add  no scripts.js a funcionalidade de todo projeto que eu quiser, apareça no portifolio, via github

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ async function carregarHabilidades() {
 }
 
 function renderizarHabilidades(lista) {
-  conteiner.innerHTML = ''; 
+  conteiner.innerHTML = '';
 
   lista.forEach((hab, index) => {
     const div = document.createElement('div');
@@ -54,3 +54,61 @@ filtros.forEach(botao => {
 });
 
 carregarHabilidades();
+
+const username = "italloferreira";
+const projeto = document.getElementById("conteiner-projeto");
+
+async function carregarRepositorio() {
+
+  const respostaURL = await fetch(`https://api.github.com/users/${username}/repos`, {
+    headers: {
+      Accept: "application/vnd.github.mercy-preview+json"
+    }
+  });
+
+  const respostaJSON = await respostaURL.json();
+
+  projeto.innerHTML = "";
+
+  respostaJSON.forEach(repositorio => {
+    if (!repositorio.topics.includes("portfolio-project")) return;
+
+    const imagem = `https://raw.githubusercontent.com/${username}/${repositorio.name}/main/print%20projeto.png`;
+
+    const cartao = document.createElement("div");
+    cartao.classList.add("projeto");
+
+    cartao.innerHTML = `
+  <div class="conteiner-projeto-img">
+    <img src="${imagem}" alt="/img/imgNaoDisponivel.png">
+  </div>
+
+  <div class="conteiner-projeto-informacao">
+    <h1>${repositorio.name}</h1>
+
+    <div class="conteiner-projeto-icones">
+      <span>${repositorio.language || "N/A"}</span>
+    </div>
+
+    <p>${repositorio.description || "Sem descrição"}</p>
+
+    <div class="conteiner-projeto-botoes">
+      <a href="${repositorio.html_url}" target="_blank">
+        <button class="butao"><img src="/img/icones/github.png"></button>
+      </a>
+
+      ${repositorio.homepage ? `
+      <a href="${repositorio.homepage}" target="_blank">
+        <button class="butao"><img src="/img/icones/external.png"></button>
+      </a>` : ""}
+    </div>
+  </div>
+`;
+    projeto.appendChild(cartao);
+
+  })
+}
+
+carregarRepositorio();
+
+
